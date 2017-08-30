dcc95e60295ccc89f6d7e1c26bf7fda44b515eea
sammkatcher <samm.i.amm.katcher@gmail.com>
2017-08-30 13:45:11 -0400

    fixing locations

--- Ballroom Website/old/404.php
+++
@@ -1,2 +0,0 @@
-We couldn't find the page: '<?php echo($wrong_page)?>'! <br /> <br />
-If you think that page should have been there, please <a href="<?php echo($PAGE_PATH);?>contact">let the webmaster know</a>.
