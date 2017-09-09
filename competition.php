6820e36926408598717c939fc02f6ac1c69fbb69
sammkatcher <samm.i.amm.katcher@gmail.com>
2017-09-09 00:18:05 -0400

    fix image issue, add 2017 results

--- competition.php
+++ competition.php
@@ -15,12 +15,12 @@
   <div class="text-center">
     <div class="row">
       <div class="col-sm-6">
-      <h3>Registration for 2017 Closed</h3>
+      <h3>Registration Coming Soon</h3>
   <a class="btn btn-default btn-ballroom disabled" href="http://www.o2cm.com/forms/entry.asp?event=rpi">Register Now!</a>
       </div>
       <div class="col-sm-6">
       <h3>Reserve spectator tickets below!</h3>
-      <a class="btn btn-default btn-ballroom" href="http://goo.gl/forms/NLAvBmdspT">Reserve Tickets</a>
+      <a class="btn btn-default btn-ballroom disabled" href="http://goo.gl/forms/NLAvBmdspT">Reserve Tickets</a>
       </div>
     </div>
   </div>
@@ -30,6 +30,8 @@
       Questions? Contact <a href="mailto:rpicompcoordinator@gmail.com"><?php echo($comp_coordinator)?></a>.</p>
     <p><br /></p>
   </div>
+  <h4> 2017 RPI DanceSport competition Results </h4>
+  <p>Thank you to everyone who attended our competition! We had a great time, and hope everyone enjoyed our Syllabus Peabody event. Results are available on <a href="http://www.o2cm.com/Results/event3.asp?event=rpi17">o2cm</a></p>
   <h4>2016 RPI DanceSport Competition Results</h4>
   <p>Thank you to everyone who attended our competition! It was one of our biggest in years! We hope everyone had a blast. Results are available on <a href="http://www.o2cm.com/Results/event3.asp?event=rpi16">o2cm</a></p>
   <h4>2015 RPI DanceSport Competition Results</h4>
