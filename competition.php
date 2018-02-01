4aaad756653ca3d0eb3d4832c70b7808aea82102
sammkatcher <samm.i.amm.katcher@gmail.com>
2018-02-01 16:59:00 -0500

    fixing other formatting

--- competition.php
+++ competition.php
@@ -35,21 +35,23 @@
   <br />
   <br />
   <br />
-  <h4> 2017 RPI DanceSport competition Results </h4>
-  <p>Thank you to everyone who attended our competition! We had a great time, and hope everyone enjoyed our Syllabus Peabody event. Results are available on <a href="http://results.o2cm.com/event3.asp?event=rpi17">o2cm</a></p>
-  <h4>2016 RPI DanceSport Competition Results</h4>
-  <p>Thank you to everyone who attended our competition! It was one of our biggest in years! We hope everyone had a blast. Results are available on <a href="http://results.o2cm.com/event3.asp?event=rpi16">o2cm</a></p>
-  <h4>2015 RPI DanceSport Competition Results</h4>
-  <p>Thanks to everyone for attending our comp. It went so smoothly and there were a good number of competitors. You can find results on <a href="http://results.o2cm.com/event3.asp?event=rpi15">o2cm</a></p>
-  <h4>2014 RPI DanceSport Competition Results</h4>
-  <p>Thanks to everyone for attending our comp. We had a lot of fun and we hope you enjoyed our new Open Bronze category. We will try to bring it back in comps to come! You can find results on <a href="http://results.o2cm.com/event3.asp?event=rpi14">o2cm</a></p>
-  <h4>The 2013 RPI Dancesport Competition results are in!</h4>
-  <p>Thank you to everyone for another wonderful competition this past spring!  After a little difficulty with our Registrar, we are happy to provide you with the Results!</p>
-  <p>2013 Results can be found: <a target="_blank" href="https://docs.google.com/spreadsheet/ccc?key=0AuJ_EZu-y_widHZ2T29ZdUhMV1F2enNDRUl4SzhFYUE&amp;usp=sharing"> HERE!</a></p>
-  <p>These were tallied manually from the judges score sheets by team members, instead.   We apologize in advance for any mistakes in final placements - we aren't professional scrutineers, but we did our best!  If you notice discrepancies please contact <a href="mailto:rpicompcoordinator@gmail.com"><?php echo($comp_coordinator)?></a> and we'll make the appropriate changes.</p>
-  <p>Again, thank you for all of your patience and we apologize for the delay.</p>
-  <h4>2011 RPI DanceSport Competition Results</h4>
-  <h4>The results have been posted <a target="_blank" href="http://www.dance.zsconcepts.com/results/rpi2012/">HERE!</a></h4>
+  <div class = "col-med-12">
+    <h4> 2017 RPI DanceSport competition Results </h4>
+    <p>Thank you to everyone who attended our competition! We had a great time, and hope everyone enjoyed our Syllabus Peabody event. Results are available on <a href="http://results.o2cm.com/event3.asp?event=rpi17">o2cm</a></p>
+    <h4>2016 RPI DanceSport Competition Results</h4>
+    <p>Thank you to everyone who attended our competition! It was one of our biggest in years! We hope everyone had a blast. Results are available on <a href="http://results.o2cm.com/event3.asp?event=rpi16">o2cm</a></p>
+    <h4>2015 RPI DanceSport Competition Results</h4>
+    <p>Thanks to everyone for attending our comp. It went so smoothly and there were a good number of competitors. You can find results on <a href="http://results.o2cm.com/event3.asp?event=rpi15">o2cm</a></p>
+    <h4>2014 RPI DanceSport Competition Results</h4>
+    <p>Thanks to everyone for attending our comp. We had a lot of fun and we hope you enjoyed our new Open Bronze category. We will try to bring it back in comps to come! You can find results on <a href="http://results.o2cm.com/event3.asp?event=rpi14">o2cm</a></p>
+    <h4>The 2013 RPI Dancesport Competition results are in!</h4>
+    <p>Thank you to everyone for another wonderful competition this past spring!  After a little difficulty with our Registrar, we are happy to provide you with the Results!</p>
+    <p>2013 Results can be found: <a target="_blank" href="https://docs.google.com/spreadsheet/ccc?key=0AuJ_EZu-y_widHZ2T29ZdUhMV1F2enNDRUl4SzhFYUE&amp;usp=sharing"> HERE!</a></p>
+    <p>These were tallied manually from the judges score sheets by team members, instead.   We apologize in advance for any mistakes in final placements - we aren't professional scrutineers, but we did our best!  If you notice discrepancies please contact <a href="mailto:rpicompcoordinator@gmail.com"><?php echo($comp_coordinator)?></a> and we'll make the appropriate changes.</p>
+    <p>Again, thank you for all of your patience and we apologize for the delay.</p>
+    <h4>2011 RPI DanceSport Competition Results</h4>
+    <h4>The results have been posted <a target="_blank" href="http://www.dance.zsconcepts.com/results/rpi2012/">HERE!</a></h4>
+  </div>
 </div>
 
 <div class="text-center">
