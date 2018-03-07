e76ffa665594bff26eeb296f6cb48c2d36f2992d
sammkatcher <samm.i.amm.katcher@gmail.com>
2018-03-07 17:12:06 -0500

    add rules to comp nav and update text on calculator

--- calculator.php
+++ calculator.php
@@ -19,5 +19,12 @@
   <button type="submit" class="btn btn-default btn-ballroom">Go!</button>
 </form>
 
-<p>Note that this uses data directly from o2cm and does not pull data from other sources (such as dance.zsconcepts.com). Also note this means that your name must be spelled <b>EXACTLY</b> like it is shown on o2cm.</p>
+<h4> Understanding YCN Points </h4>
+<p>The YCN point system determines eligibility for the Bronze, Silver, Gold, and Pre-Champ levels.</p>
+<ul>
+  <li>To be eligible to compete you must have less than 7 points in the dance at that level.</li>
+  <li>If you have pointed out of a dance that is linked to another, you cannot dance in either of them (e.g. if a rhythm leader has 6 points in chacha, rumba, and mambo, but has 7 points in swing, he cannot complete in Silver Swing/Mambo—because they are linked events. He may still compete in Silver Chacha/Rumba, and can double register in Gold rhythm if the competition allows double registration. If double registration is prohibited, he can only dance Gold rhythm.)</li>
+ </ul>
+<br/>
+<p>Note that this uses data directly from o2cm and does not pull data from other sources (such as dance.zsconcepts.com). Also note this means that your name must be spelled <strong>EXACTLY</strong> like it is shown on o2cm.</p>
 <p>Note that if your name is listed with an apostrophe, use a grave accent (a.k.a. back-tick, typically on the same key as the tilde, ~) instead.</p>
