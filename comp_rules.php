4cb55601231f3228b3cac221ede018e34435a0f4
sammkatcher <samm.i.amm.katcher@gmail.com>
2018-03-07 17:19:14 -0500

    fix double bullets on rules

--- comp_rules.php
+++ comp_rules.php
@@ -9,35 +9,35 @@
   <p></p>
 
   <li>Double registration (i.e. registering for subsequent levels in the same style) is allowed.</li>
-  <li>&bull; Couples must dance at the level of the higher competitor (i.e. if the Follower is Bronze, and the Leader is Silver, the couple must dance at the Silver level and have the option of double registering to also compete at the Gold level, if they so desire). </li>
-  <li>&bull; There are <strong>no gender restrictions</strong> on couples.</li>
+  <li>Couples must dance at the level of the higher competitor (i.e. if the Follower is Bronze, and the Leader is Silver, the couple must dance at the Silver level and have the option of double registering to also compete at the Gold level, if they so desire). </li>
+  <li>There are <strong>no gender restrictions</strong> on couples.</li>
 </ul>
 
   <h4>Level Eligability</h4>
     <ul>
-      <li>&bull; Newcomer: Have not danced prior to the Spring 2018 semester.</li>
-      <li>&bull; YCN points should be used to determine level eligibility for Bronze, Silver, Gold, and Open/Pre-Champ. Please use our <a href="calculator">YCN point calculator</a> to determine level eligibility.
+      <li>Newcomer: Have not danced prior to the Spring 2018 semester.</li>
+      <li>YCN points should be used to determine level eligibility for Bronze, Silver, Gold, and Open/Pre-Champ. Please use our <a href="calculator">YCN point calculator</a> to determine level eligibility.
       </li>
       <li>
         <ul>
-        <li>&bull; To be eligible to compete you must have less than 7 points in the dance at that level. </li>
-        <li>&bull; If you have pointed out of a dance that is linked to another, you cannot dance in either of them (e.g. if a rhythm leader has 6 points in chacha, rumba, and mambo, but has 7 points in swing, he cannot complete in Silver Swing/Mambo—because they are linked events. He may still compete in Silver Chacha/Rumba, and is welcome to double register for the gold rhythm events.)</li>
+        <li>To be eligible to compete you must have less than 7 points in the dance at that level. </li>
+        <li>If you have pointed out of a dance that is linked to another, you cannot dance in either of them (e.g. if a rhythm leader has 6 points in chacha, rumba, and mambo, but has 7 points in swing, he cannot complete in Silver Swing/Mambo—because they are linked events. He may still compete in Silver Chacha/Rumba, and is welcome to double register for the gold rhythm events.)</li>
         </ul>
       </li>
       </li>
-      <li>&bull; Syllabus events are open to all competitors.</li>
+      <li>Syllabus events are open to all competitors.</li>
     </ul>
   </li>
   <h4>Syllabi</h4>
   <ul>
-    <li>&bull; Competitors should follow the ISTD syllabus for International styles and the NDCA syllabus for American style dances for Newcomer, Bronze, Silver, and Gold levels.</li>
-    <li>&bull; Open/ Pre-Champ competitors are not restricted to a syllabus but lifts will not be permitted.</li>
+    <li>Competitors should follow the ISTD syllabus for International styles and the NDCA syllabus for American style dances for Newcomer, Bronze, Silver, and Gold levels.</li>
+    <li>Open/ Pre-Champ competitors are not restricted to a syllabus but lifts will not be permitted.</li>
   </ul>
   <h4>Costumes</h4>
   <ul>
-    <li>&bull; Costumes are permitted at the Bronze level and above.</li>
-    <li>&bull; Heel protectors (plastic or suede) are required for all high-heeled shoes.</li>
+    <li>Costumes are permitted at the Bronze level and above.</li>
+    <li>Heel protectors (plastic or suede) are required for all high-heeled shoes.</li>
   </ul>
   <p></p>
-  <p>&bull; All is fair in love and <del>war</del> Fun Dances.</p>
+  <p>All is fair in love and <del>war</del> Fun Dances.</p>
   
