0426df907a112c6abe0a1cba04437ce0921ae06f
Samm <samm.i.amm.katcher@gmail.com>
2018-04-12 16:46:09 -0400

    try latin example

---
+++ results_rpi_2018_latin.php
@@ -0,0 +1,621 @@
+<?php include('comp_nav.php')?>
+
+<script>
+  document.getElementById("comp_nav_events").className = "active";
+</script>
+
+<h1>Latin Results</h1>
+<h2> The Quarter Final and Semi Final data is the Number of Callbacks in that round. </h2>
+<div id="accordion">
+    <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Newcomer
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+      <h3>Cha Cha</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>First Round</th>
+            <th>Quarter Final</th>
+            <th>Semi Final</th>
+            <th>Final Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><strong>233</strong></td>
+            <td> - </td>
+            <td> 3 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>333</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 1 </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>340</strong></td>
+            <td> - </td>
+            <td> 5 </td>
+            <td> 5 </td>
+            <td><strong>2</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>347</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 2 </td>
+            <td><strong>7</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>359</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 3 </td>
+            <td><strong>5</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>373</strong></td>
+            <td> - </td>
+            <td>  </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>374</strong></td>
+            <td> - </td>
+            <td> 2 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>375</strong></td>
+            <td> - </td>
+            <td>  </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>380</strong></td>
+            <td> - </td>
+            <td> 5 </td>
+            <td> 2 </td>
+            <td><strong>6</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>383</strong></td>
+            <td> - </td>
+            <td> 2 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>391</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 3 </td>
+            <td><strong>3</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>393</strong></td>
+            <td> - </td>
+            <td> 5 </td>
+            <td> 5 </td>
+            <td><strong>4</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>394</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 1 </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>396</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 1 </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>397</strong></td>
+            <td> - </td>
+            <td> 1 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>387</strong></td>
+            <td> - </td>
+            <td>5 </td>
+            <td>5</td>
+            <td><strong>1</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>378</strong></td>
+            <td> - </td>
+            <td>4 </td>
+            <td>2</td>
+            <td><strong>8</strong></td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Rumba</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>First Round</th>
+            <th>Quarter Final</th>
+            <th>Semi Final</th>
+            <th>Final Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><strong>387</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 5 </td>
+            <td><strong>1</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>393</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 5 </td>
+            <td><strong>2</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>340</strong></td>
+            <td> - </td>
+            <td> 4 </td>
+            <td> 4 </td>
+            <td><strong>4</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>359</strong></td>
+            <td> - </td>
+            <td> 3 </td>
+            <td> 5 </td>
+            <td><strong>5</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>391</strong></td>
+            <td> - </td>
+            <td> 2 </td>
+            <td> 5 </td>
+            <td><strong>4</strong></td>
+          </tr>
+
+      <tr>
+            <td><strong>347</strong></td>
+            <td> - </td>
+            <td> 2 </td>
+            <td> 3 </td>
+            <td><strong>6</strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>396</strong></td>
+            <td> - </td>
+            <td> 3 </td>
+            <td> 1 </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>383</strong></td>
+            <td> - </td>
+            <td> 2 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>233</strong></td>
+            <td> - </td>
+            <td> 3 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>378</strong></td>
+            <td> - </td>
+            <td> 2 </td>
+            <td> 3 </td>
+            <td><strong>7</strong></td>
+          </tr>
+
+      <tr>
+            <td><strong>380</strong></td>
+            <td> - </td>
+            <td> 3 </td>
+            <td> 2 </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>333</strong></td>
+            <td> - </td>
+            <td> 2 </td>
+            <td> 1 </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>373</strong></td>
+            <td> - </td>
+            <td> 2 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr> 
+
+          <tr>
+            <td><strong>394</strong></td>
+            <td> - </td>
+            <td> 1 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>397</strong></td>
+            <td> - </td>
+            <td> - </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+          <tr>
+            <td><strong>374</strong></td>
+            <td> - </td>
+            <td> 1 </td>
+            <td> </td>
+            <td><strong> </strong></td>
+          </tr>
+
+        </tbody>
+      </table>
+      <h3>Jive</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>First Round</th>
+            <th>Quarter Final</th>
+            <th>Semi Final</th>
+            <th>Final Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td><strong>233</strong></td>
+              <td> - </td>
+              <td> 4 </td>
+              <td> 2 </td>
+              <td><strong> </strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>333</strong></td>
+              <td> - </td>
+              <td> 3 </td>
+              <td> </td>
+              <td><strong> </strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>340</strong></td>
+              <td> - </td>
+              <td> 4 </td>
+              <td> 4 </td>
+              <td><strong>2</strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>359</strong></td>
+              <td> - </td>
+              <td> 5 </td>
+              <td> 5 </td>
+              <td><strong>5</strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>373</strong></td>
+              <td> - </td>
+              <td>  </td>
+              <td> </td>
+              <td><strong> </strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>374</strong></td>
+              <td> - </td>
+              <td> 5 </td>
+              <td> </td>
+              <td><strong> </strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>375</strong></td>
+              <td> - </td>
+              <td> 5 </td>
+              <td> 4 </td>
+              <td><strong>6</strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>380</strong></td>
+              <td> - </td>
+              <td> 5 </td>
+              <td> 3 </td>
+              <td><strong>3</strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>383</strong></td>
+              <td> - </td>
+              <td> 4 </td>
+              <td> </td>
+              <td><strong> </strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>391</strong></td>
+              <td> - </td>
+              <td> 5 </td>
+              <td> 5 </td>
+              <td><strong>4</strong></td>
+            </tr>
+
+
+            <tr>
+              <td><strong>393</strong></td>
+              <td> - </td>
+              <td> 5 </td>
+              <td> 5 </td>
+              <td><strong>1</strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>394</strong></td>
+              <td> - </td>
+              <td> 2 </td>
+              <td> </td>
+              <td><strong> </strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>397</strong></td>
+              <td> - </td>
+              <td> 4 </td>
+              <td> </td>
+              <td><strong> </strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>387</strong></td>
+              <td> - </td>
+              <td> 3 </td>
+              <td> </td>
+              <td><strong> </strong></td>
+            </tr>
+
+            <tr>
+              <td><strong>378</strong></td>
+              <td> - </td>
+              <td> 5 </td>
+              <td> 2 </td>
+              <td><strong> </strong></td>
+            </tr>
+      
+        </tbody>
+      </table>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Bronze
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+      <h3>Waltz</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>First Round</th>
+            <th>Quarter Final</th>
+            <th>Semi Final</th>
+            <th>Final Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><strong>Couple</strong></td>
+            <td>First Round Recalls, if no first round put - </td>
+            <td>Quarter Final Recalls, if no qf put - </td>
+            <td>Semi Final Recalls</td>
+            <td><strong>Final Placement</strong></td>
+          </tr>
+          <tr>
+            <td><strong>Couple</strong></td>
+            <td>First Round Recalls, if no first round put - </td>
+            <td>Quarter Final Recalls, if no qf put - </td>
+            <td>Semi Final Recalls</td>
+            <td><strong>Final Placement</strong></td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Quickstep</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>First Round</th>
+            <th>Quarter Final</th>
+            <th>Semi Final</th>
+            <th>Final Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><strong>Couple</strong></td>
+            <td>First Round Recalls, if no first round put - </td>
+            <td>Quarter Final Recalls, if no qf put - </td>
+            <td>Semi Final Recalls</td>
+            <td><strong>Final Placement</strong></td>
+          </tr>
+          <tr>
+            <td><strong>Couple</strong></td>
+            <td>First Round Recalls, if no first round put - </td>
+            <td>Quarter Final Recalls, if no qf put - </td>
+            <td>Semi Final Recalls</td>
+            <td><strong>Final Placement</strong></td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Tango</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>First Round</th>
+            <th>Quarter Final</th>
+            <th>Semi Final</th>
+            <th>Final Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><strong>Couple</strong></td>
+            <td>First Round Recalls, if no first round put - </td>
+            <td>Quarter Final Recalls, if no qf put - </td>
+            <td>Semi Final Recalls</td>
+            <td><strong>Final Placement</strong></td>
+          </tr>
+          <tr>
+            <td><strong>Couple</strong></td>
+            <td>First Round Recalls, if no first round put - </td>
+            <td>Quarter Final Recalls, if no qf put - </td>
+            <td>Semi Final Recalls</td>
+            <td><strong>Final Placement</strong></td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Foxtrot</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>First Round</th>
+            <th>Quarter Final</th>
+            <th>Semi Final</th>
+            <th>Final Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><strong>Couple</strong></td>
+            <td>First Round Recalls, if no first round put - </td>
+            <td>Quarter Final Recalls, if no qf put - </td>
+            <td>Semi Final Recalls</td>
+            <td><strong>Final Placement</strong></td>
+          </tr>
+          <tr>
+            <td><strong>Couple</strong></td>
+            <td>First Round Recalls, if no first round put - </td>
+            <td>Quarter Final Recalls, if no qf put - </td>
+            <td>Semi Final Recalls</td>
+            <td><strong>Final Placement</strong></td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Silver
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+      <h3>Waltz</h3>
+      <h3>Quickstep</h3>
+      <h3>Tango</h3>
+      <h3>Foxtrot</h3>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Gold
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+      <h3>Waltz</h3>
+      <h3>Quickstep</h3>
+      <h3>Tango</h3>
+      <h3>Foxtrot</h3>
+    </div>
+  </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Syllabus
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+      <h3>Viennese Waltz</h3>
+    </div>
+  </div>
+</div>
