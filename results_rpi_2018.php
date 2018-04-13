6a7aea0c315d99a999300cfbb5c79597413d42ae
Samm <samm.i.amm.katcher@gmail.com>
2018-04-13 15:41:04 -0400

    tweaking the styles nav

--- results_rpi_2018.php
+++ results_rpi_2018.php
@@ -1,9 +1,9 @@
 <div class="text-center">
   <h1>2018 Competition Results</h1>
-  <div class="btn-group btn-group-lg" role="group">
-    <button type="button" class="btn btn-default btn-ballroom"><a href="/results_rpi_2018_standard">Standard</a></button>
-    <button type="button" class="btn btn-default btn-ballroom"><a href="/results_rpi_2018_smooth">Smooth</a></button>
-    <button type="button" class="btn btn-default btn-ballroom"><a href="/results_rpi_2018_rhythm">Rhythm</a></button>
-    <button type="button" class="btn btn-default btn-ballroom"><a href="/results_rpi_2018_latin">Latin</a></button>
+  <div class="btn-group btn-group-lg btn-group-justified" role="group">
+    <a type="button" class="btn btn-default btn-ballroom" href="/results_rpi_2018_standard">Standard</a></button>
+    <a type="button" class="btn btn-default btn-ballroom" href="/results_rpi_2018_smooth">Smooth</a></button>
+    <a type="button" class="btn btn-default btn-ballroom" href="/results_rpi_2018_rhythm">Rhythm</a></button>
+    <a type="button" class="btn btn-default btn-ballroom" href="/results_rpi_2018_latin">Latin</a></button>
   </div>
 </div>
