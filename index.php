9f84ecdfb23856a9c59090c7325709b81f86a93c
Samm <samm.i.amm.katcher@gmail.com>
2018-08-28 16:12:11 -0400

    adding social dance to top bar

--- index.php
+++ index.php
@@ -47,9 +47,10 @@
 	      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
 		<li><a href="/ballroom-latin">Ballroom & Latin</a></li>
-		<li><a href="/tango">Argentine Tango</a></li>
+		<li><a href="/tango"><s>Argentine Tango</s></a></li>
 		<li><a href="/swing">Lindy Hop Swing</a></li>
     <li><a href="/salsa">Salsa</a></li>
+    <li><a href="/socialdance">Social Dancing</a></li>
 		<li class="divider"></li>
 		<li><a href="/officers">Officers</a></li>
 		<!-- <li><a href="#">History</a></li> -->
